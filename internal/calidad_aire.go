cc35166a50a492d14db71c7bc48fd6f35c63dc14
lmchaves <lmchaveslop@gmail.com>
2024-11-11 15:45:30 +0100

    feat(M0): closes pablotl0#8
    Creo la estructura principal DatosCalidadAire, con el formato de medición que se especifíca en la página de Miteco proporcionada, para ello re han relaizado nuvas estructuras de datos como: la Ubicación, el Contaminante y las diferentes estructuras en función de su periodicidad de medición

---
+++ internal/calidad_aire.go
@@ -0,0 +1,53 @@
+package internal
+
+import (
+    "time"
+)
+
+//Para la localización precisa de la región específica
+type Ubicacion struct {
+    Provincia   string
+    Municipio   string
+    IDEstacion  string  
+    Latitud     float64 
+    Longitud    float64 
+}
+
+// Representación de un contaminante
+type Contaminante struct {
+    Magnitud        string  
+    Concentracion   float64 
+    Unidad          string  
+    Cumplimiento    bool    
+}
+
+
+// La estructura de los datos es diferente en función de su periodicidad
+
+type DatosHorario struct {
+    Fecha          time.Time    
+    Hora           int          
+    Contaminantes  []Contaminante 
+}
+
+type DatosDiario struct {
+    Fecha          time.Time     
+    Contaminantes  []Contaminante 
+}
+
+type DatosIrregular struct {
+    FechaInicial   time.Time      
+    FechaFinal     time.Time      
+    Contaminantes  []Contaminante 
+}
+
+// Es la estructura de datos principal
+type DatosCalidadAire struct {
+    Ubicacion         Ubicacion       
+    MetodoEvaluacion  string          
+    FrecuenciaMuestreo string         
+    DatosHorario      []DatosHorario  
+    DatosDiario       []DatosDiario   
+    DatosIrregular    []DatosIrregular 
+    Fuente            string          
+}
