1b6e9a928f77e4eeea03fba10c2d78b1282c8cb1
lmchaves <lmchaveslop@gmail.com>
2024-11-12 15:33:52 +0100

    feat(M0): closes pablotl0#8
    Dentro de la estructura DatosCalidadAire creada, he modificado la Fuente para que en lugar de un string sea de la estructura FuenteDatos creada en pablotl0#9

--- internal/calidad_aire.go
+++ internal/calidad_aire.go
@@ -1,53 +1,52 @@
 package internal
 
 import (
-    "time"
+	"time"
 )
 
 //Para la localización precisa de la región específica
 type Ubicacion struct {
-    Provincia   string
-    Municipio   string
-    IDEstacion  string  
-    Latitud     float64 
-    Longitud    float64 
+	Provincia  string
+	Municipio  string
+	IDEstacion string
+	Latitud    float64
+	Longitud   float64
 }
 
 // Representación de un contaminante
 type Contaminante struct {
-    Magnitud        string  
-    Concentracion   float64 
-    Unidad          string  
-    Cumplimiento    bool    
+	Magnitud      string
+	Concentracion float64
+	Unidad        string
+	Cumplimiento  bool
 }
-
 
 // La estructura de los datos es diferente en función de su periodicidad
 
 type DatosHorario struct {
-    Fecha          time.Time    
-    Hora           int          
-    Contaminantes  []Contaminante 
+	Fecha         time.Time
+	Hora          int
+	Contaminantes []Contaminante
 }
 
 type DatosDiario struct {
-    Fecha          time.Time     
-    Contaminantes  []Contaminante 
+	Fecha         time.Time
+	Contaminantes []Contaminante
 }
 
 type DatosIrregular struct {
-    FechaInicial   time.Time      
-    FechaFinal     time.Time      
-    Contaminantes  []Contaminante 
+	FechaInicial  time.Time
+	FechaFinal    time.Time
+	Contaminantes []Contaminante
 }
 
 // Es la estructura de datos principal
 type DatosCalidadAire struct {
-    Ubicacion         Ubicacion       
-    MetodoEvaluacion  string          
-    FrecuenciaMuestreo string         
-    DatosHorario      []DatosHorario  
-    DatosDiario       []DatosDiario   
-    DatosIrregular    []DatosIrregular 
-    Fuente            string          
+	Ubicacion          Ubicacion
+	MetodoEvaluacion   string
+	FrecuenciaMuestreo string
+	DatosHorario       []DatosHorario
+	DatosDiario        []DatosDiario
+	DatosIrregular     []DatosIrregular
+	Fuente             FuenteDatos
 }
