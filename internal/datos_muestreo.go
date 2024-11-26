752c3028f1b28b7020073b53e910b9f49ecfbe1b
lmchaves <lmchaveslop@gmail.com>
2024-11-26 11:07:05 +0100

    fix(M0): elimino fehcaFinal #8
    En datos_muestreo.go se ha elminiado fechaFinal ya que puede dar lugar a complicaciones como,¿cómo se interpreta un valor diario si las fechas abarcan varios días? Entonces se pasa a utilizar una única fecha  como representación diaria simplificando el diseño

--- internal/datos_muestreo.go
+++ internal/datos_muestreo.go
@@ -8,7 +8,6 @@ import (
 // MuestraCalidadAire representa una entrada de datos de calidad del aire
 type MuestraCalidadAire struct {
 	FechaInicial  time.Time               // Fecha de inicio del muestreo 
-	FechaFinal    time.Time               // Fecha de fin de muestreo 
 	Contaminantes map[string]Contaminante // Contaminantes mapeados por su magnitud
 }
 
@@ -19,14 +18,12 @@ var (
 )
 
 // Constructor para MuestraCalidadAire
-func NewMuestraCalidadAire(fechaInicial, fechaFinal time.Time, contaminantes []Contaminante) (MuestraCalidadAire, error) {
+func NewMuestraCalidadAire(fechaInicial time.Time, contaminantes []Contaminante) (MuestraCalidadAire, error) {
 	// Validar rango de fechas
 	if fechaInicial.Before(fechaMinima) || fechaInicial.After(fechaMaxima) {
 		return MuestraCalidadAire{}, errors.New("la fecha inicial debe estar entre el año 2000 y el presente")
 	}
-	if fechaFinal.Before(fechaInicial) || fechaFinal.After(fechaMaxima) {
-		return MuestraCalidadAire{}, errors.New("la fecha final debe estar después de la inicial y dentro del rango válido")
-	}
+
 
 	// Validar contaminantes
 	if len(contaminantes) == 0 {
@@ -46,7 +43,6 @@ func NewMuestraCalidadAire(fechaInicial, fechaFinal time.Time, contaminantes []C
 
 	return MuestraCalidadAire{
 		FechaInicial:  fechaInicial,
-		FechaFinal:    fechaFinal,
 		Contaminantes: contaminantesMap,
 	}, nil
 }
