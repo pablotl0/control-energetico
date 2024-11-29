c2f784e479059fc5e98393664c23e726bdae8196
lmchaves <lmchaveslop@gmail.com>
2024-11-29 11:15:50 +0100

    feat(M0): modifico el array #8
    Se ha cambiado el array de DatosMuestreo por un map, para una representación mas adecueada

--- internal/calidad_aire.go
+++ internal/calidad_aire.go
@@ -2,11 +2,12 @@ package internal
 
 import (
     "errors"
+    "time"
 )
 
 type DatosCalidadAire struct {
     Ubicacion     Ubicacion
-    DatosMuestreo []MuestraCalidadAire 
+    DatosMuestreo map[time.Time]MuestraCalidadAire
 }
 
 func NewDatosCalidadAire(ubicacion Ubicacion, datosMuestreo []MuestraCalidadAire) (DatosCalidadAire, error) {
@@ -14,5 +15,13 @@ func NewDatosCalidadAire(ubicacion Ubicacion, datosMuestreo []MuestraCalidadAire
         return DatosCalidadAire{}, errors.New("debe haber al menos un dato de muestreo")
     }
 
-    return DatosCalidadAire{Ubicacion: ubicacion, DatosMuestreo: datosMuestreo}, nil
+    datosMap := make(map[time.Time]MuestraCalidadAire)
+	for _, muestra := range datosMuestreo {
+		if _, exists := datosMap[muestra.FechaInicial]; exists {
+			return DatosCalidadAire{}, errors.New("datos duplicados para la misma fecha")
+		}
+		datosMap[muestra.FechaInicial] = muestra
+	}
+
+    return DatosCalidadAire{Ubicacion: ubicacion, DatosMuestreo: datosMap}, nil
 }
