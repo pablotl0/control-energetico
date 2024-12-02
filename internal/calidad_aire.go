6c46498758b4809a87a2c7b82c06ae0b02ec3df2
lmchaves <lmchaveslop@gmail.com>
2024-12-02 11:36:07 +0100

    fix(M0): arreglo el constructor para usar Fecha como clave en DatosMuestreo
    He actualizado el constructor  para usar correctamente la estructura  como clave en el mapa . Esto garantiza que los datos se puedan acceder por año, mes o día, y mejora la consistencia del código.

--- internal/calidad_aire.go
+++ internal/calidad_aire.go
@@ -24,22 +24,20 @@ func NewMuestreoCalidadAire(provincia string, datosMuestreo []MuestraCalidadAire
 		return nil, errors.New("debe haber al menos un dato de muestreo")
 	}
 
-	datosMap := make(map[int]map[int]map[int]MuestraCalidadAire)
+	datosMap := make(map[Fecha]MuestraCalidadAire)
 
-	for _, muestra := range datosMuestreo {
-		year, month, day := muestra.FechaInicial.Date()
-		
-		if _, ok := datosMap[year]; !ok {
-			datosMap[year] = make(map[int]map[int]MuestraCalidadAire)
-		}
-		if _, ok := datosMap[year][int(month)]; !ok {
-			datosMap[year][int(month)] = make(map[int]MuestraCalidadAire)
-		}
-		if _, exists := datosMap[year][int(month)][day]; exists {
-			return nil, fmt.Errorf("datos duplicados para la fecha: %04d-%02d-%02d", year, month, day)
+    for _, muestra := range datosMuestreo {
+		fecha := Fecha{
+			Año: muestra.FechaInicial.Year(),
+			Mes: int(muestra.FechaInicial.Month()), 
+			Día: muestra.FechaInicial.Day(),
 		}
 
-		datosMap[year][int(month)][day] = muestra
+		if _, exists := datosMap[fecha]; exists {
+			return nil, fmt.Errorf("datos duplicados para la fecha: %04d-%02d-%02d", fecha.Año, fecha.Mes, fecha.Día)
+		}
+
+		datosMap[fecha] = muestra
 	}
 
 	return &MuestreoCalidadAire{
